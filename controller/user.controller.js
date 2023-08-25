7d36b204ecf11051ecfc7726a19079249e7b8e34
Abduljebar Sani <Abduljebar49@gmail.com>
2023-08-25 22:38:07 +0300

    update added

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -34,6 +34,25 @@ const addUser = (req, res) => {
   });
 };
 
+const updateUser = (req, res) => {
+  const { id } = req.body;
+  const { name, password, email } = req.body;
+  var user;
+  Users.map((ele) => {
+    if (id == ele.id) {
+      user = ele;
+      ele.name = name;
+      ele.password = password;
+      ele.email = email;
+    }
+  });
+  res.status(200).send({
+    model: user,
+    message: "Successfully fetched",
+    statusCode: 200,
+  });
+};
+
 const userWithEmail = (req, res) => {
   const { email } = req.body;
 
@@ -50,4 +69,5 @@ module.exports = {
   getUser,
   addUser,
   userWithEmail,
+  updateUser
 };
--- routes/users.router.js
+++ routes/users.router.js
@@ -4,12 +4,14 @@ const {
   getUser,
   addUser,
   userWithEmail,
+  updateUser,
 } = require("../controller/user.controller");
 var router = express.Router();
 
 router.get("/users", getUsers);
 router.get("/user/:id", getUser);
 router.post("/user", addUser);
+router.patch("/user/:id", updateUser);
 router.post("/user/email", userWithEmail);
 
 module.exports = router;
